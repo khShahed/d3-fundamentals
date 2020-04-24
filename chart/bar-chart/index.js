76be00ce29a8bf766467958245ad5a6c64020efe
Kamrul Hasan Shahed <khshahed90@gmail.com>
2020-04-25 02:12:03 +0600

    color and label added to bar chart

--- chart/bar-chart/index.js
+++ chart/bar-chart/index.js
@@ -2,11 +2,16 @@ const width = 300;
 const height = 300;
 const padding = 2;
 
-const dataset = [50, 10, 15, 20, 25, 14];
+const dataset = [50, 10, 15, 20, 25, 14, 18, 60, 45, 20, 5, 34];
 
 const svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height);
+
+function colorPicker(value){
+  if (value <= 20) return "#666666";
+  else if (value > 20) return "#FF0033";
+}
 
 svg.selectAll("rect")
   .data(dataset)
@@ -16,9 +21,30 @@ svg.selectAll("rect")
       return index * (width / dataset.length);
     })
     .attr("y", function (data) {
-      return height - data;
+      return height - (data * 4);
     })
     .attr("width", width / dataset.length - padding)
     .attr("height", function (data) {
-      return data;
+      return data * 4;
+    })
+    .attr("fill", function (data) {
+      return colorPicker(data);
     });
+
+svg.selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function (data) {
+    return data;
+  })
+  .attr("text-anchor", "middle")
+  .attr("x", function (data, index) {
+    return  index * (width / dataset.length) + (width / dataset.length - padding) / 2;
+  })
+  .attr("y",  function (data) {
+    return height - (data * 4) - 10;
+  })
+  .attr("font-family", "sans-serif")
+  .attr("font-size", 12)
+  .attr("fill", "blue");
