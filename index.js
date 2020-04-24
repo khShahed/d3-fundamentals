0fda4c63bfa45970abbeac4cb94b67d2fefe82da
Kamrul Hasan Shahed <khshahed90@gmail.com>
2020-04-25 00:50:12 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+d3.select("body")
+  .append("svg")
+    .attr("width", "90vw")
+    .attr("height", "90vh")
+  .append("text")
+    .text("D3 Fundamentals")
+    .attr("y", "5%")
+    .attr("x", "40%")
+    .style("fill", "blue")
+    .style("font-weight", "bold")
+    .style("word-spacing", "15px")
+    .style("letter-spacing", "3px")
+    .style("font-size", "25px");
