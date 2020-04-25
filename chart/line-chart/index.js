0650f3ef76a098fc41e0e8af8e8dac750d0282ce
Kamrul Hasan Shahed <khshahed90@gmail.com>
2020-04-25 15:35:32 +0600

    all curves of line chart added

--- chart/line-chart/index.js
+++ chart/line-chart/index.js
@@ -1,7 +1,6 @@
 const width = 1000;
 const height = 600;
 
-const backgroundColor = "#AAAFFF";
 const lineColor = "#F2845C";
 const textColor = "#30E7D2";
 const dotColor = "#E7188F";
@@ -31,8 +30,7 @@ const lineFunc = d3.line()
 const svg = d3.select("body")
   .append("svg")
   .attr("width", width)
-  .attr("height", height)
-  .attr("fill", backgroundColor);
+  .attr("height", height);
 
 const lineChart = svg.append("path")
   .attr("d", lineFunc(dataset))
@@ -56,6 +54,7 @@ const labels = svg.selectAll("text")
   .attr("fill", textColor)
   .style("z-index", 2);
 
+// Add dot
 const dots = svg.selectAll("dot")
   .data(dataset)
   .enter()
@@ -68,3 +67,72 @@ const dots = svg.selectAll("dot")
     return height - data.earning;
   })
   .attr("fill", dotColor);
+
+// All curve types from http://using-d3js.com/05_04_curves.html
+const curveArray = [
+  {"d3Curve":d3.curveLinear,"curveTitle":"curveLinear"},
+  {"d3Curve":d3.curveLinearClosed,"curveTitle":"curveLinearClosed"},
+  
+  {"d3Curve":d3.curveStep,"curveTitle":"curveStep"},
+  {"d3Curve":d3.curveStepBefore,"curveTitle":"curveStepBefore"},
+  {"d3Curve":d3.curveStepAfter,"curveTitle":"curveStepAfter"},
+  
+  {"d3Curve":d3.curveBundle,"curveTitle":"curveBundle"},
+  {"d3Curve":d3.curveBundle.beta(0),"curveTitle":"curveBundle beta 0"},
+  {"d3Curve":d3.curveBundle.beta(0.5),"curveTitle":"curveBundle beta 0.5"},
+  {"d3Curve":d3.curveBundle.beta(1),"curveTitle":"curveBundle beta 1"},
+  
+  {"d3Curve":d3.curveNatural,"curveTitle":"curveNatural"},
+  
+  {"d3Curve":d3.curveBasis,"curveTitle":"curveBasis"},
+  {"d3Curve":d3.curveBasisOpen,"curveTitle":"curveBasisOpen"},
+  {"d3Curve":d3.curveBasisClosed,"curveTitle":"curveBasisClosed"},
+  
+  {"d3Curve":d3.curveCardinal,"curveTitle":"curveCardinal"},
+  {"d3Curve":d3.curveCardinalOpen,"curveTitle":"curveCardinalOpen"},
+  {"d3Curve":d3.curveCardinalClosed,"curveTitle":"curveCardinalClosed"},
+  {"d3Curve":d3.curveCatmullRom,"curveTitle":"curveCatmullRom"},
+  {"d3Curve":d3.curveCatmullRomOpen,"curveTitle":"curveCatmullRomOpen"},
+  {"d3Curve":d3.curveCatmullRomClosed,"curveTitle":"curveCatmullRomClosed"},
+  
+  {"d3Curve":d3.curveMonotoneX,"curveTitle":"curveMonotoneX"},
+  {"d3Curve":d3.curveMonotoneY,"curveTitle":"curveMonotoneY"},
+];
+const numberOfCurve = curveArray.length;
+const intervalTime = 1000;
+const curveTitleId = "line-chart-name";
+let index = 0;
+
+setInterval(() => {
+  // Change curve type
+  lineFunc.curve(curveArray[index].d3Curve);
+  
+  // Remove existing chart path
+  svg.selectAll("path").remove();
+  
+  // Add data again to render new path
+  svg.append("path")
+    .attr("d", lineFunc(dataset))
+    .attr("stroke", lineColor)
+    .attr("stroke-width", 4)
+    .attr("fill", "none");
+  
+  // Remove existing curve title
+  svg.select('#'+curveTitleId).remove();
+  
+  // Add new curve title
+  svg.append("text")
+    .text(curveArray[index].curveTitle)
+    .attr("y", 30)
+    .attr("x", 200)
+    .style("fill", "blue")
+    .style("font-weight", "bold")
+    .style("word-spacing", "15px")
+    .style("letter-spacing", "3px")
+    .style("font-size", "25px")
+    .attr("id", curveTitleId);
+  
+  index++;
+  // if we're on the end of array then restart
+  if (index === numberOfCurve) index = 0;
+}, intervalTime);
